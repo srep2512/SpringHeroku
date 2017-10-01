68247d09f756fd911345fda5c1a772761603a985
Felix Brandt <ms861418@campussachsen.onmicrosoft.com>
2017-10-01 16:42:50 +0200

    initial

---
+++ HerokuTest/src/main/java/io/javabrains/HerokuTestApplication.java
@@ -0,0 +1,12 @@
+package io.javabrains;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HerokuTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HerokuTestApplication.class, args);
+	}
+}
